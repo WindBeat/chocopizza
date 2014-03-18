0bb2eef79542f4c366cc60bbc5ea4dc7c0d33f4d
WindBeat <jordanwalker465@gmail.com>
2014-03-18 16:53:45 -0700

    added check class

--- application.js
+++ application.js
@@ -1,3 +1,7 @@
 $(document).on("click", ".box", function() {
 	$(this).closest('div').toggleClass("check")
 });
+
+$(document).on("click", ".check", function() {
+	$(this).closest('div').toggleClass("box")
+});
