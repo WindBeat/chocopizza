213f6590e5f908f5ac24fa863238c7af347c548f
WindBeat <jordanwalker465@gmail.com>
2014-03-18 16:54:23 -0700

    Update application.js

--- application.js
+++ application.js
@@ -1,7 +1,3 @@
 $(document).on("click", ".box", function() {
 	$(this).closest('div').toggleClass("check")
 });
-
-$(document).on("click", ".check", function() {
-	$(this).closest('div').toggleClass("box")
-});
